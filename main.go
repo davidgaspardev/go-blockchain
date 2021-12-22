52ce160eb1bd26d8ed9abe470f9dd036566cb6b6
David Gaspar <davidgaspar.dev@gmail.com>
2021-12-22 09:35:53 -0500

    Testing the basic blockchain

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	blockchain "go-blockchain/src"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	blockchain := blockchain.InitBlockchain()
+	start := time.Now()
+	for i := 1; i <= 5000; i++ {
+		blockchain.AddBlock([]byte(fmt.Sprintf("I'm %dº block", i)))
+	}
+	elapsed := time.Since(start)
+	blockchain.ShowChain()
+
+	fmt.Printf("Time taken to create blocks: %s\n", elapsed)
 }
